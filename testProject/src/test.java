ee554b7e3b9c34ff28a573fc1b107b973e82a5ed
Kevin Dichter <kevinidichter@gmail.com>
2025-01-31 19:57:31 -0500

    testing my own commit

---
+++ testProject/src/test.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+/*
+ * The most important class, the class with main in it.
+ */
+public class test {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        try (Scanner console = new Scanner (System.in)){
+            String input = console.nextLine();
+            System.out.println(input);
+        }
+    }
+}
+
