1477b8a4616b7f200a39e0591ebc64153eb41b2e
Lincoln <lincolncraddock@gmail.com>
2025-02-11 19:20:47 -0500

    added color to CLI View

--- src/CLIView.java
+++ src/CLIView.java
@@ -5,19 +5,40 @@ import java.util.Scanner;
 public class CLIView implements View
 {
 
+	// fields
     private Scanner sc = new Scanner(System.in);
+    
+    // color related fields, constants, etc
+    public final String TOGGLE_COLOR_COMMAND = "toggle color";
+    public final String TOGGLE_COLOR_SHORTHAND_COMMAND = "tc";
+    
+    private boolean color = false;
+    private String failStyle = "";
+    private String successStyle = "";
+    private String displayStyle = "";
+    private String stopStyle = "";
+    
+    private final String RED = "\u001B[31m";
+    private final String GREEN = "\u001B[32m";
+    private final String RESET = "\u001B[0m";
+    private final String ITALLICS = "\u001B[3m";
+    
 
     @Override
     public Command nextCommand() 
     {
-        Command c = Command.parseCommand(sc.nextLine());
+    	String in = sc.nextLine();
+        Command c = Command.parseCommand(in);
         while(c == null)
         {
-            System.out.println("Invalid Command");
-            c = Command.parseCommand(sc.nextLine());
+        	if (in.equals("toggle color") || in.equals("tc"))
+        		toggleColor();
+        	else
+        		System.out.println("Invalid Command");
+        	in = sc.nextLine();
+            c = Command.parseCommand(in);
         }
         return c;
-        
     }
 
     @Override
@@ -25,7 +46,6 @@ public class CLIView implements View
     {
         System.out.println(message);
         return sc.nextLine();
-
     }
 
     @Override
@@ -51,6 +71,7 @@ public class CLIView implements View
             while(!checkMsg.equals("")) // This loop will keep prompting the user until they input something that satisfies the check
             {
                 ans = promptForInput(checkMsg);
+                checkMsg = checks.get(i).check(ans);
             }
             result.add(ans);
         }
@@ -60,24 +81,47 @@ public class CLIView implements View
     @Override
     public void notifySuccess() 
     {
-        System.out.println("Successful command");
+        // System.out.println("Successful command"); // don't print anything
     }
 
     @Override
     public void notifySuccess(String message) 
     {
-        System.out.println(message);
+        System.out.println(successStyle + message + stopStyle);
     }
 
     @Override
     public void notifyFail(String message) 
     {
-        System.out.println(message);
+        System.out.println(failStyle + message + stopStyle);
     }
 
     @Override
     public void display(String message) {
-        System.out.println(message);
+        System.out.println(displayStyle + message + stopStyle);
+    }
+    
+    
+    /**
+     * Turns on/off color text, and other visual niceties in the terminal.
+     */
+    private void toggleColor()
+    {
+    	color = ! color;
+    	if (color)
+    	{
+    		failStyle = RED + ITALLICS;
+    		successStyle = GREEN + ITALLICS;
+    		displayStyle = ITALLICS;
+    		stopStyle = RESET;
+    	}
+    	else
+    	{
+    		failStyle = "";
+    		successStyle = "";
+    		displayStyle = "";
+    		stopStyle = "";
+    	}
     }
     
 }
