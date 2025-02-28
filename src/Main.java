b64eede69682b7e2bb27f0d127060c0a7c6360f8
Tim King <163905685+TimKing35@users.noreply.github.com>
2025-02-27 21:10:38 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,26 @@
-
 /**
  * The Main class for the entire UML editor program.
  * @author Lincoln Craddock
  */
 public class Main {
 
-	public static void main(String[] args) {
-		
-		Controller controller = new Controller(new CLIView(), new JModel());
-		controller.run();
-	}
+    public static void main(String[] args) {
+        if (hasCLIFlag(args)){
+            Controller controller = new Controller(new CLIView(), new JModel());
+            controller.run();
+        } else {
+			// NEEDS TO BE UPDATED TO LAUNCH GUI AND NOT CLI
+            Controller controller = new Controller(new CLIView(), new JModel());
+            controller.run();
+        }
+    }
 
+	public static boolean hasCLIFlag(String[] args) {
+		for (String arg : args) {
+			if (arg.equalsIgnoreCase("--cli")) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
