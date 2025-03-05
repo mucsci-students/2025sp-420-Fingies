b60577115e8d55fecfcd9c3bedf3530e91e08054
Lincoln <lincolncraddock@gmail.com>
2025-03-05 10:53:04 -0500

    added methods to remove a class and rename a class in the GUI and added randomly generated unique colors for each class

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@
  */
 public class Main {
 
+<<<<<<< HEAD
     public static void main(String[] args) {
         if (hasCLIFlag(args)){
             Controller controller = new Controller(new CLIView(), new JModel());
@@ -14,6 +15,21 @@ public class Main {
             controller.run();
         }
     }
+=======
+	public static void main(String[] args) {
+		GUIView view = new GUIView();
+		Controller controller = new Controller(view, new JModel());
+		view.setController(controller);
+		view.addUMLClass("Class 1");
+		view.addUMLClass("Class 2");
+		view.addUMLClass("Class 3");
+
+		view.removeUMLClass("Class 2");
+
+		view.renameUMLClass("Class 1", "ClassA");
+		view.run();
+	}
+>>>>>>> f472cba (added methods to remove a class and rename a class in the GUI and added randomly generated unique colors for each class)
 
 	public static boolean hasCLIFlag(String[] args) {
 		for (String arg : args) {
