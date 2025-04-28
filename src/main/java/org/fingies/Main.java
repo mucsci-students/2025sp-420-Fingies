2fd8e0152511b1425f708cd4032b65b04f56e6ea
Lincoln <lincolncraddock@gmail.com>
2025-04-27 22:56:19 -0400

    removed default classes from GUI

--- src/main/java/org/fingies/Main.java
+++ src/main/java/org/fingies/Main.java
@@ -28,58 +28,6 @@ public class Main {
             GUIView view = new GUIView();
             UMLController controller = new UMLController(view, new JModel());
             view.setController(controller);
-            // view.run();
-
-            String class1 = "ClassA";
-            String class2 = "ClassB";
-            String class3 = "zzzz";
-
-            UMLClassHandler.createClass(class1);
-            UMLClassHandler.createClass(class2);
-            UMLClassHandler.createClass(class3);
-
-            // Add fields and methods (if needed)
-            UMLClassHandler.getClass(class1).addField("FieldA", "String");
-            UMLClassHandler.getClass(class1).addField("FieldB", "String");
-            UMLClassHandler.getClass(class1).addField("FieldC", "String");
-            UMLClassHandler.getClass(class1).addField("FieldD", "String");
-            UMLClassHandler.getClass(class1).addField("FieldE", "String");
-            UMLClassHandler.getClass(class1).addField("FieldF", "String");
-
-           // UMLClassHandler.getClass(class2).addField("FieldA");
-           // UMLClassHandler.getClass(class2).addField("FieldAaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-           // Create a method in Class A (optional)
-           ArrayList<String> params = new ArrayList<>();
-           ArrayList<String> types = new ArrayList<>();
-           
-           UMLClassHandler.getClass(class3).addMethod("MethodC", "void", params, types);
-
-           params.add("Param1");
-           params.add("Param2");
-           params.add("Param3");
-           types.add("String");
-           types.add("String");
-           types.add("String");
-           UMLClassHandler.getClass(class2).addMethod("MethodB", "void", params, types);
-
-           params.add("Param4");
-           params.add("Param5");
-           types.add("String");
-           types.add("String");
-           UMLClassHandler.getClass(class1).addMethod("MethodA", "void", params, types);
-
-           // Add the UML classes to the view
-           view.addUMLClass(class1);
-           view.addUMLClass(class2);
-           view.addUMLClass(class3);
-
-           // Add relationships between classes to test arrows
-           RelationshipHandler.addRelationship(class1, class3, RelationshipType.Inheritance);
-           RelationshipHandler.addRelationship(class3, class1, RelationshipType.Composition);
-           RelationshipHandler.addRelationship(class2, class3, RelationshipType.Aggregation);
-           RelationshipHandler.addRelationship(class1, class1, RelationshipType.Realization);
-           RelationshipHandler.addRelationship(class3, class3, RelationshipType.Composition);
-           RelationshipHandler.addRelationship(class3, class2, RelationshipType.Realization);
 
            view.updateArrows();  // Call this to refresh and redraw arrows
            view.run();     
